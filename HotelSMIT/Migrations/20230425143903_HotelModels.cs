e386b3e1c53aad6d06a6bf3abb406030d14d1f47
MrSanderSi <sihversass@gmail.com>
2023-04-25 17:47:24 +0300

    Database model changes done

---
+++ HotelSMIT/Migrations/20230425143903_HotelModels.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelSMIT.Migrations
+{
+    /// <inheritdoc />
+    public partial class HotelModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HotelRoom",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomNumber = table.Column<int>(type: "int", nullable: false),
+                    BedCount = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Enabled = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelRoom", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookedRoom",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookedRoom", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookedRoom_HotelRoom_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "HotelRoom",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookedRoom_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedRoom_RoomId",
+                table: "BookedRoom",
+                column: "RoomId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookedRoom_UserId",
+                table: "BookedRoom",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookedRoom");
+
+            migrationBuilder.DropTable(
+                name: "HotelRoom");
+        }
+    }
+}
