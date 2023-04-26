0c1bc0183fe547ab30912a26d03cb283f2134807
MrSanderSi <sihversass@gmail.com>
2023-04-26 17:46:54 +0300

    Simplified version of adding data - missing toast and proper visuals

--- HotelSMIT/Pages/ManagementTools.razor.cs
+++ HotelSMIT/Pages/ManagementTools.razor.cs
@@ -1,22 +1,27 @@
 ﻿using HotelSMIT.Controllers;
 using HotelSMIT.Data;
 using HotelSMIT.Data.Models;
+using HotelSMIT.UI.Models;
 
 namespace HotelSMIT.Pages
 {
 	public partial class ManagementTools
 	{
-		public HotelRoom Room { get; set; }
+		public HotelRoom Room { get; set; } = new();
+
+		RoomModel RoomModel { get; set; } = new();
 
 		ManagementService _ManagementService = new();
 
-		private async void AddRoom(int roomNumber, int bedCount, decimal Price)
+		private async void AddRoom()
 		{
-			HotelRoom newRoom = new HotelRoom();
-			newRoom.RoomNumber = roomNumber;
-			newRoom.BedCount = bedCount;
-			newRoom.Price = Price;
-			newRoom.Enabled = true;
+			HotelRoom newRoom = new()
+			{
+				RoomNumber = RoomModel.RoomNumber,
+				BedCount = RoomModel.BedCount,
+				Price = RoomModel.Price,
+				Enabled = true
+			};
 
 			await _ManagementService.AddNewRoom(newRoom);
 		}
---
+++ HotelSMIT/UI/Models/RoomModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelSMIT.UI.Models
+{
+	public class RoomModel
+	{
+		[Required]
+		[Range(1,800)]
+		public int RoomNumber { get; set; }
+
+		[Required]
+		[Range(1,3)]
+		public int BedCount { get; set; }
+
+		[Required]
+		public decimal Price { get; set; }
+	}
+}
